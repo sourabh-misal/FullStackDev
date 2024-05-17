c72c780847a0a9fae1341916794b2b68777752ba
sourabh-misal <sourabhmisal182@gamil.com>
2024-05-18 00:34:03 +0530

    Week3.1 middleware

---
+++ week3/middle.js
@@ -0,0 +1,19 @@
+const express = require('express')
+
+const app = express();
+
+app.get('/get-health', (req,res) => {
+    const username = req.headers.username;
+    const password = req.headers.password;
+//    const kidneyId = req.query.kidneykId;
+
+    if(username === "sourabh" && password === "pass"){
+        res.status(200).json({
+            msg: " you kidney is fine",
+        });
+    }
+
+   res.status(404).json({msg :"wrong input",});
+})
+
+app.listen(3000);
